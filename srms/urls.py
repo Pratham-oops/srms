281c9b45410e716d9220e34476e9a8b644473d42
Srushti <srushtisshah69@gmail.com>
2023-07-16 22:39:39 -0400

    first commit

---
+++ srms/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for srms project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+#Edited the Admin Side Lookout
+admin.site.site_header = "SRMS Admin"
+admin.site.site_title = "SRMS Admin Portal"
+admin.site.index_title = "Welcome to SRMS Researcher Portal"
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),
+]
